a6523965a0db7e14a94762eb49e8971384a9fc99
snowgecko <kate.wykes@gmail.com>
2022-02-16 09:55:41 +0000

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+let cacheName = "pwa-app-native";
+let filesToCache = 
+["/","./", "./index.html", "./pages.html","./manifest.json", 
+"./css/pages.css",
+"./css/site.css",
+"./css/pages.css"
+]; //removed "./js/main.js"
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
